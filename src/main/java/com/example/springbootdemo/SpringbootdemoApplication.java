9b51ee3ab59721a2cb6c52f5981420baffc10766
IMANE ZAAD <104379762+zdimane@users.noreply.github.com>
2025-01-19 22:55:02 +0100

    Update SpringbootdemoApplication.java

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -1,37 +1,34 @@
-package com.example.springbootdemo;
+package com.example.springbootdemo;  
 
-import com.example.springbootdemo.dao.entities.Product;
-import com.example.springbootdemo.dao.repositories.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import com.example.springbootdemo.dao.entities.Product;  
+import com.example.springbootdemo.dao.repositories.ProductRepository;  
+import org.springframework.beans.factory.annotation.Autowired;  
+import org.springframework.boot.CommandLineRunner;  
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.List;  
 
-import java.util.List;
+@SpringBootApplication  
+public class SpringbootdemoApplication implements CommandLineRunner {  
 
+    private final ProductRepository productRepository;  
 
+    // Utilisation de l'injection par constructeur  
+    @Autowired  
+    public SpringbootdemoApplication(ProductRepository productRepository) {  
+        this.productRepository = productRepository;  
+    }  
 
+    public static void main(String[] args) {  
+        SpringApplication.run(SpringbootdemoApplication.class, args);  
+    }  
 
-
-@SpringBootApplication
-public class SpringbootdemoApplication implements CommandLineRunner {
-
-
-	@Autowired
-	private ProductRepository productRepository ;
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Product pro = new Product(1,"xproce","xxxxxxxxxxxx",13);
-		productRepository.save(pro);
-
-		List<Product> students = productRepository.findAll();
-		System.out.println(students);
-
-
-	}
+    @Override  
+    public void run(String... args) throws Exception {  
+        Product pro = new Product(1,"xproce","xxxxxxxxxxxx",13);  
+        productRepository.save(pro);  
+        List<Product> products = productRepository.findAll();  
+        System.out.println(products);  
+    }  
 }
