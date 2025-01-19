6a2025c49ea940c2b51384ce84b87730394bba31
IMANE ZAAD <104379762+zdimane@users.noreply.github.com>
2025-01-19 22:57:57 +0100

    Update SpringbootdemoApplication.java

--- src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringbootdemoApplication.java
@@ -2,6 +2,8 @@ package com.example.springbootdemo;
 
 import com.example.springbootdemo.dao.entities.Product;  
 import com.example.springbootdemo.dao.repositories.ProductRepository;  
+import org.slf4j.Logger;  
+import org.slf4j.LoggerFactory;  
 import org.springframework.beans.factory.annotation.Autowired;  
 import org.springframework.boot.CommandLineRunner;  
 import org.springframework.boot.SpringApplication;  
@@ -13,8 +15,8 @@ import java.util.List;
 public class SpringbootdemoApplication implements CommandLineRunner {  
 
     private final ProductRepository productRepository;  
+    private static final Logger logger = LoggerFactory.getLogger(SpringbootdemoApplication.class);  
 
-    // Utilisation de l'injection par constructeur  
     @Autowired  
     public SpringbootdemoApplication(ProductRepository productRepository) {  
         this.productRepository = productRepository;  
@@ -29,6 +31,8 @@ public class SpringbootdemoApplication implements CommandLineRunner {
         Product pro = new Product(1,"xproce","xxxxxxxxxxxx",13);  
         productRepository.save(pro);  
         List<Product> products = productRepository.findAll();  
-        System.out.println(products);  
+        
+        // Remplacement de System.out par un logger  
+        logger.info("Liste des produits : {}", products);  
     }  
 }
